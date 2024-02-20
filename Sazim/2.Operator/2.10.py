33946fe0739ef72f5ecb6a9f957563ff8de0305e
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2024-02-21 00:35:41 +0600

    bitwise operator

---
+++ Sazim/2.Operator/2.10.py
@@ -0,0 +1,19 @@
+
+# 2.10. Write a program that read two numbers and display bitwise AND
+
+
+
+
+
+
+# Python Bitwise operator
+# & - Bitwise And
+# | - Bitwise OR
+# ^ - Bitwise Exclusive OR - XOR
+# ~ - Bitwise NOT
+# >> - Bitwise Left Shift
+# >> - Bitwise Right Shift
+
+
+# Binary numbers
+# 
