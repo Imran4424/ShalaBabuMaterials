e4e6e0e57a2df1f05a4db61f97988e1b363ff76e
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2024-02-29 23:29:57 +0600

    conditional logic

---
+++ Sazim/4.ConditionalLogic/4.1.1.py
@@ -0,0 +1,12 @@
+
+# 4.1. Write a program that read an integer and prints odd or even
+
+number = int(input("enter an int number: "))
+
+# if  this condition is true
+if number % 2 == 0:
+    # then it will run this code block
+    print("Even")
+else: # if false
+    # then it will run this code block
+    print("Odd")
---
+++ Sazim/4.ConditionalLogic/4.1.py
@@ -0,0 +1,19 @@
+
+# 4.1. Write a program that read an integer and prints odd or even
+
+number = int(input("enter an int number: "))
+
+# if condition is true
+if number % 2 == 0:
+    print("Even") # then it will run this code block
+    
+# if condition is true
+if number % 2 == 1:
+    print("Odd")  # then it will run this code block
+
+
+
+# if you divide somethin with 2
+# 
+# if the remainder is 1 that is Odd.
+# if the remainder is 0 that is Even
