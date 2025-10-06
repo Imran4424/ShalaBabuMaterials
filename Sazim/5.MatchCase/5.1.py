2b8a3e760121fc8516fe3a7b4ea00b4685fa8e25
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2025-10-06 19:40:00 +0600

    match case

---
+++ Sazim/5.MatchCase/5.1.py
@@ -0,0 +1,28 @@
+# 5.1. Write a program that read a digit and diplay by spelling
+
+number = int(input("enter an int number: "))
+
+match number:
+	case 0:
+		print("Zero")
+	case 1:
+		print("One")
+	case 2:
+		print("Two")
+	case 3:
+		print("Three")
+	case 4:
+		print("Four")
+	case 5:
+		print("Five")
+	case 6:
+		print("Six")
+	case 7:
+		print("Seven")
+	case 8:
+		print("Eight")
+	case 9:
+		print("Nine")
+	case _:
+		print("Not a digit")
+
---
+++ Sazim/5.MatchCase/IfIntuition.py
@@ -0,0 +1,26 @@
+# 5.1. Write a program that read a digit and diplay by spelling
+
+number = int(input("enter an int number: "))
+
+if number == 0:
+	print("Zero")
+elif number == 1:
+	print("One")
+elif number == 2:
+	print("Two")
+elif number == 3:
+	print("Three")
+elif number == 4:
+	print("Four")
+elif number == 5:
+	print("Five")
+elif number == 6:
+	print("Six")
+elif number == 7:
+	print("Seven")
+elif number == 8:
+	print("Eight")
+elif number == 9:
+	print("Nine")
+else:
+	print("Not a digit")
