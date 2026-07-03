7bebbddf6097d5baeefc5b21966233caae657d21
SHAH MD. IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2026-07-03 23:20:15 +0600

    input output

--- Sazim/C/1.InputOutput/1.3.c
+++ Sazim/C/1.InputOutput/1.3.c
@@ -4,6 +4,9 @@
 
 int main()
 {
+	// Disable stdout buffering completely
+	setvbuf(stdout, NULL, _IONBF, 0);
+
 	printf("Please enter an interger number: ");
 	int num; // variable declaration
 	scanf("%d", &num); // taking intput of the variable
